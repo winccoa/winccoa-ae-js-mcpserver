1986d26e9926a032e2230f373e51b2244fbcca08
Markus Übeleis <office@cope-it.at>
2025-07-23 11:33:16 +0200

    Revert "Add configuration via dp"
    This reverts commit 0e40953ca70b9ae07728ce1b74eee8070b3b9589.

--- mcpWinCCOA/src/index_http.js
+++ mcpWinCCOA/src/index_http.js
@@ -1,34 +1,31 @@
 /*******************************************************/
 /*                                                     */
 /*   This file was initially creates by Martin Kumhera */
-/*   and extended by AI with CNS (UNS) functions!      */
+/*   and extended by AI with CNS (UNS) functions!     */
 /*                                                     */
 /*******************************************************/
+
+
 import { StreamableHTTPServerTransport } from "@modelcontextprotocol/sdk/server/streamableHttp.js";
 import express from 'express';
-import fs from 'fs';
-import https from 'https';
-import State from "./tools/stateManager.js";
-import { init_tools } from './tools/tool_oa.js';
-import { WinccoaManager } from 'winccoa-manager'; 
+import { init_tools } from './tool_oa.js';
 
-const winccoa = new WinccoaManager();
-const instance = await new State().init(winccoa);
+/* Random token generation*/
+import crypto from 'crypto';
 
-const options = {
-  key: fs.readFileSync(instance.getState().keyPath), 
-  cert: fs.readFileSync(instance.getState().certPath)
-};
+const API_TOKEN = '1a89f5420cc0f92318b29552198592e4'; //crypto.randomBytes(16).toString('hex');
 
-const server = init_tools(winccoa);
+const server = init_tools();
+
+// ==================== EXPRESS SERVER SETUP ====================
+
 const app = express();
-console.log(instance.getState().mainInstructions);
-console.log(instance.getState().specificInstructions);
 app.use(express.json());
 
 app.post('/mcp', async (req, res) => {
-  const token = req.headers['authorization'];
-  if (token !== instance.getState().token) {
+  const token = req.headers['authorization'] || req.body?.token;
+  console.log(token);
+  if (token !== API_TOKEN) {
     return res.status(401).json({
       jsonrpc: '2.0',
       error: {
@@ -89,7 +86,8 @@ app.delete('/mcp', async (req, res) => {
   }));
 });
 
+// Start the server
 const PORT = 3000;
-https.createServer(options, app).listen(PORT, () => {
-  console.log(`HTTPS server running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`MCP Extended WinCC OA Server with CNS/UNS listening on port ${PORT}`);
 });
